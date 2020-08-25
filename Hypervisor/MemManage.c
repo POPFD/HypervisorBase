d96a5570d3ba82bcf5b9a4cf349aff7b8f9a5061
POPFD <22840694+POPFD@users.noreply.github.com>
2020-08-25 16:28:26 +0100

    Commit of broken state, re-factoring MemManage to use a better method of reading guest/host memory.

--- Hypervisor/MemManage.c
+++ Hypervisor/MemManage.c
@@ -1,7 +1,6 @@
 #include <intrin.h>
 #include <ntddk.h>
 #include "MemManage.h"
-#include "Paging.h"
 #include "ia32.h"
 
 /******************** External API ********************/
@@ -9,168 +8,86 @@
 
 /******************** Module Typedefs ********************/
 
+/* Each level of a paging structure. */
+typedef enum
+{
+	PT_LEVEL_PTE = 1,
+	PT_LEVEL_PDE = 2,
+	PT_LEVEL_PDPTE = 3,
+	PT_LEVEL_PML4E = 4
+} PT_LEVEL;
+
 
 /******************** Module Constants ********************/
 #define OFFSET_DIRECTORY_TABLE_BASE 0x028
 #define OFFSET_USER_DIR_TABLE 0x280
 
+#define SIZE_2MB (2 * 1024 * 1024)
+
+/* Counts of how many entries are in a table. */
+#define PAGING_TABLE_ENTRY_COUNT 512
+#define PAGING_PML4E_COUNT		512
+#define PAGING_PML3E_COUNT		512
+#define PAGING_PML2E_COUNT		512
+#define PAGING_PML1E_COUNT		512
+
+/* Calculates the offset into the PDE (PML1) structure. */
+#define ADDRMASK_PML1_OFFSET(_VAR_) ((SIZE_T)_VAR_ & 0xFFFULL)
+
+/* Calculates the index of the PDE (PML1) within the PDT structure. */
+#define ADDRMASK_PML1_INDEX(_VAR_) (((SIZE_T)_VAR_ & 0x1FF000ULL) >> 12)
+
+/* Calculates the index of the PDT (PML2) within the PDPT*/
+#define ADDRMASK_PML2_INDEX(_VAR_) (((SIZE_T)_VAR_ & 0x3FE00000ULL) >> 21)
+
+/* Calculates the index of the PDPT (PML3) within the PML4 */
+#define ADDRMASK_PML3_INDEX(_VAR_) (((SIZE_T)_VAR_ & 0x7FC0000000ULL) >> 30)
+
+/* Calculates the index of PML4. */
+#define ADDRMASK_PML4_INDEX(_VAR_) (((SIZE_T)_VAR_ & 0xFF8000000000ULL) >> 39)
+
 /******************** Module Variables ********************/
 
 
 /******************** Module Prototypes ********************/
+static NTSTATUS split2MbPage(PDE_2MB_64* pdeLarge);
+static PT_ENTRY_64* getPTEFromVA(CR3 tableBase, PVOID virtualAddress, PT_LEVEL* level);
+static UINT64 physicalFromVirtual(VOID* virtualAddress);
+static VOID* virtualFromPhysical(UINT64 physicalAddress);
 
 /******************** Public Code ********************/
 
-NTSTATUS MemManage_changeMemoryProt(PEPROCESS process, PUINT8 baseAddress, SIZE_T size, BOOLEAN writable, BOOLEAN executable)
+NTSTATUS MemManage_init(PMM_CONTEXT context, CR3 hostCR3)
 {
-	NTSTATUS status = STATUS_UNSUCCESSFUL;
+	NTSTATUS status = STATUS_SUCCESS;
 
-	PUINT8 startVA = PAGE_ALIGN(baseAddress);
-	PUINT8 endVA = baseAddress + size;
-
-	for (PUINT8 currentVA = startVA; currentVA < endVA; currentVA += PAGE_SIZE)
+	/* Reserve a single page, this will be used for mapping in the guest 
+	 * page data into. */
+	context->reservedPage = MmAllocateMappingAddress(PAGE_SIZE, 0);
+	if (NULL != context->reservedPage)
 	{
-		/* Get the physical address of the page table entry. */
-		PHYSICAL_ADDRESS physPTE;
-		status = MemManage_getPTEPhysAddressFromVA(process, currentVA, &physPTE);
+		/* Attempt to get the page table entry of the reserved page,
+		 * we need to ensure this is not a 2MB large page, if so we must split it. */
+		PT_LEVEL tableLevel;
+		PT_ENTRY_64* reservedPagePTE = getPTEFromVA(hostCR3, context->reservedPage, &tableLevel);
+		if (PT_LEVEL_PDE == tableLevel)
+		{
+			/* A split must take place. */
+			status = split2MbPage((PDE_2MB_64*)reservedPagePTE);
+
+			/* Get the new PTE. */
+			reservedPagePTE = getPTEFromVA(hostCR3, context->reservedPage, &tableLevel);
+		}
+
+		/* Ensure we are still in success state, splitting could have failed. */
 		if (NT_SUCCESS(status))
 		{
-			/* Read the original page table entry. */
-			PTE readPTE;
 
-			status = MemManage_readPhysicalAddress(physPTE, sizeof(readPTE), &readPTE);
-			if (NT_SUCCESS(status))
-			{
-				/* Modify the page table entry with new flags. */
-				readPTE.ReadWrite = writable;
-				readPTE.ExecuteDisable = (FALSE == executable);
-
-				/* Write the page table entry. */
-				status = MemManage_writePhysicalAddress(physPTE, sizeof(readPTE), &readPTE);
-				if (NT_ERROR(status))
-				{
-					/* Error detected so break from the loop. */
-					break;
-				}
-			}
 		}
 	}
-
-	return status;
-}
-
-NTSTATUS MemManage_readVirtualAddress(PEPROCESS targetProcess, void* sourceVA, SIZE_T size, void* targetVA)
-{
-	NTSTATUS status;
-
-	/* Get the physical address of the source. */
-	PHYSICAL_ADDRESS targetSource;
-	status = MemManage_getPhysFromVirtual(targetProcess, sourceVA, &targetSource);
-	if (NT_SUCCESS(status))
+	else
 	{
-		status = MemManage_readPhysicalAddress(targetSource, size, targetVA);
-	}
-
-	return status;
-}
-
-NTSTATUS MemManage_writeVirtualAddress(PEPROCESS targetProcess, void* targetVA, SIZE_T size, void* sourceVA)
-{
-	NTSTATUS status;
-
-	/* Get the physical address of the target. */
-	PHYSICAL_ADDRESS targetPhysical;
-	status = MemManage_getPhysFromVirtual(targetProcess, targetVA, &targetPhysical);
-	if (NT_SUCCESS(status))
-	{
-		status = MemManage_writePhysicalAddress(targetPhysical, size, sourceVA);
-	}
-
-	return status;
-}
-
-NTSTATUS MemManage_getPhysFromVirtual(PEPROCESS targetProcess, PVOID virtualAddress, PHYSICAL_ADDRESS* physicalAddress)
-{
-	/* Find the physical address of the PTE for the virtual address. */
-	PHYSICAL_ADDRESS physPTE;
-	NTSTATUS status = MemManage_getPTEPhysAddressFromVA(targetProcess, virtualAddress, &physPTE);
-	if (NT_SUCCESS(status))
-	{
-		/* Read the PTE. */
-		PTE readPTE;
-		status = MemManage_readPhysicalAddress(physPTE, sizeof(readPTE), &readPTE);
-		if (NT_SUCCESS(status))
-		{
-			/* Convert from page number to physical address. */
-			physicalAddress->QuadPart = (readPTE.PageFrameNumber * PAGE_SIZE) + ADDRMASK_PML1_OFFSET(virtualAddress);
-		}
-	}
-
-	return status;
-}
-
-NTSTATUS MemManage_getPTEPhysAddressFromVA(PEPROCESS targetProcess, PVOID virtualAddress, PHYSICAL_ADDRESS* physPTE)
-{
-	NTSTATUS status;
-
-	/* Get the base address of the paging table/PML4. */
-	UINT64 tableBase = MemManage_getPageTableBase(targetProcess);
-
-	/* Gather the indexes for the page tables from the VA. */
-	UINT64 indexPML4 = ADDRMASK_PML4_INDEX(virtualAddress);
-	UINT64 indexPML3 = ADDRMASK_PML3_INDEX(virtualAddress);
-	UINT64 indexPML2 = ADDRMASK_PML2_INDEX(virtualAddress);
-	UINT64 indexPML1 = ADDRMASK_PML1_INDEX(virtualAddress);
-
-	/* Read PML4 from the guest. */
-	PHYSICAL_ADDRESS physPML4;
-	physPML4.QuadPart = (LONGLONG)&((PPML4E)tableBase)[indexPML4];
-	PML4E readPML4;
-
-	status = MemManage_readPhysicalAddress(physPML4, sizeof(readPML4), &readPML4);
-	if (NT_SUCCESS(status))
-	{
-		if (TRUE == readPML4.Present)
-		{
-			/* Read PML3 from the guest. */
-			PHYSICAL_ADDRESS physPML3;
-			physPML3.QuadPart = (LONGLONG)&((PPDPTE)(readPML4.PageFrameNumber * PAGE_SIZE))[indexPML3];
-			PDPTE readPML3;
-
-			status = MemManage_readPhysicalAddress(physPML3, sizeof(readPML3), &readPML3);
-			if (NT_SUCCESS(status))
-			{
-				if (TRUE == readPML3.Present)
-				{
-					/* Read PML2 from the guest. */
-					PHYSICAL_ADDRESS physPML2;
-					physPML2.QuadPart = (LONGLONG)&((PPDE)(readPML3.PageFrameNumber * PAGE_SIZE))[indexPML2];
-					PDE readPML2;
-
-					status = MemManage_readPhysicalAddress(physPML2, sizeof(readPML2), &readPML2);
-					if (NT_SUCCESS(status))
-					{
-						if (TRUE == readPML2.Present)
-						{
-							/* Read PML1E from the guest. */
-							physPTE->QuadPart = (LONGLONG)&((PPTE)(readPML2.PageFrameNumber * PAGE_SIZE))[indexPML1];
-						}
-						else
-						{
-							status = STATUS_INVALID_ADDRESS;
-						}
-					}
-				}
-				else
-				{
-					status = STATUS_INVALID_ADDRESS;
-				}
-			}
-		}
-		else
-		{
-			status = STATUS_INVALID_ADDRESS;
-		}
+		status = STATUS_INSUFFICIENT_RESOURCES;
 	}
 
 	return status;
@@ -205,37 +122,107 @@ UINT64 MemManage_getPageTableBase(PEPROCESS process)
 	return tableBase;
 }
 
-NTSTATUS MemManage_readPhysicalAddress(PHYSICAL_ADDRESS sourceAddress, SIZE_T size, void* targetAddress)
-{
-	MM_COPY_ADDRESS copyAddress;
-	copyAddress.PhysicalAddress = sourceAddress;
 
-	SIZE_T bytesTransferred;
-	return MmCopyMemory(targetAddress, copyAddress, size, MM_COPY_MEMORY_PHYSICAL, &bytesTransferred);
-}
+/******************** Module Code ********************/
 
-NTSTATUS MemManage_writePhysicalAddress(PHYSICAL_ADDRESS targetAddress, SIZE_T size, void* sourceAddress)
+static NTSTATUS split2MbPage(PDE_2MB_64* pdeLarge)
 {
 	NTSTATUS status;
 
-	/* Attempt to map the physical memory into this address space. */
-	PVOID mappedTarget = MmMapIoSpace(targetAddress, size, MmNonCached);
-	if (NULL != mappedTarget)
+	/* Allocate a new page table, this will be used for splitting the 2MB
+	 * entry into 512 4kb pages, 512 8byte entries = one page. */
+	PTE_64* pt = MmAllocateMappingAddress(PAGE_SIZE, 0);
+	if (NULL != pt)
 	{
-		/* Copy to the mapped area. */
-		RtlCopyMemory(mappedTarget, sourceAddress, size);
+		/* Close the large page bit and then propagate the current permissions to
+		 * all the entries in our newly allocated PT. */
+		pdeLarge->LargePage = FALSE;
+		__stosq((UINT64*)pt, pdeLarge->Flags, PAGING_TABLE_ENTRY_COUNT);
 
-		/* Unmap the mapped physical address. */
-		MmUnmapIoSpace(mappedTarget, size);
+		/* Calculate the physical address of where the PDPTE will be. */
+		UINT64 base = pdeLarge->PageFrameNumber * SIZE_2MB;
 
+		/* Update the page frame mapping for each PTE. */
+		for (SIZE_T i = 0; i < PAGING_TABLE_ENTRY_COUNT; i++)
+		{
+			UINT64 physAddrToMap = base + (i * PAGE_SIZE);
+			pt[i].PageFrameNumber = physAddrToMap / PAGE_SIZE;
+		}
+
+		/* Now update the convert the largePDE to a standard entry. */
+		PDE_64* pde = (PDE_64*)pdeLarge;
+		pde->Reserved1 = 0;
+		pde->Reserved2 = 0;
+		pde->PageFrameNumber = physicalFromVirtual(pt) / PAGE_SIZE;
 		status = STATUS_SUCCESS;
 	}
 	else
 	{
-		status = STATUS_NO_MEMORY;
+		status = STATUS_INSUFFICIENT_RESOURCES;
 	}
-
+	
 	return status;
 }
 
-/******************** Module Code ********************/
+static PT_ENTRY_64* getPTEFromVA(CR3 tableBase, PVOID virtualAddress, PT_LEVEL* level)
+{
+	PT_ENTRY_64* result = NULL;
+
+	/* Gather the indexes for the page tables from the VA. */
+	UINT64 indexPML4 = ADDRMASK_PML4_INDEX(virtualAddress);
+	UINT64 indexPML3 = ADDRMASK_PML3_INDEX(virtualAddress);
+	UINT64 indexPML2 = ADDRMASK_PML2_INDEX(virtualAddress);
+	UINT64 indexPML1 = ADDRMASK_PML1_INDEX(virtualAddress);
+
+	/* Read the PML4 from the target. */
+	PML4E_64* pml4 = virtualFromPhysical(tableBase.AddressOfPageDirectory * PAGE_SIZE);
+	PML4E_64* pml4e = &pml4[indexPML4];
+	if (TRUE == pml4e->Present)
+	{
+		/* Indicate we have atleast got the entry. */
+		result = (PT_ENTRY_64*)pml4e;
+		*level = PT_LEVEL_PML4E;
+
+		/* Read PML3 from the guest. */
+		PDPTE_64* pdpt = virtualFromPhysical(pml4e->PageFrameNumber * PAGE_SIZE);
+		PDPTE_64* pdpte = &pdpt[indexPML3];
+		if (TRUE == pdpte->Present)
+		{
+			result = (PT_ENTRY_64*)pdpte;
+			*level = PT_LEVEL_PDPTE;
+
+			/* Read PML2 from the guest. */
+			PDE_64* pd = virtualFromPhysical(pdpte->PageFrameNumber * PAGE_SIZE);
+			PDE_64* pde = &pd[indexPML2];
+			if ((FALSE == pdpte->LargePage) && (TRUE == pde->Present))
+			{
+				result = (PT_ENTRY_64*)pde;
+				*level = PT_LEVEL_PDE;
+
+				/* Read PML1 from the guest. */
+				PTE_64* pt = virtualFromPhysical(pde->PageFrameNumber * PAGE_SIZE);
+				PTE_64* pte = &pt[indexPML1];
+				if ((FALSE == pde->LargePage) && (TRUE == pte->Present))
+				{
+					result = (PT_ENTRY_64*)pte;
+					*level = PT_LEVEL_PTE;
+				}
+			}
+		}
+	}
+
+	return result;
+}
+
+static UINT64 physicalFromVirtual(VOID* virtualAddress)
+{
+	return MmGetPhysicalAddress(virtualAddress).QuadPart;
+}
+
+static VOID* virtualFromPhysical(UINT64 physicalAddress)
+{
+	PHYSICAL_ADDRESS pa;
+	pa.QuadPart = physicalAddress;
+
+	return MmGetVirtualForPhysical(pa);
+}
--- Hypervisor/MemManage.h
+++ Hypervisor/MemManage.h
@@ -1,18 +1,16 @@
 #pragma once
 #include <wdm.h>
+#include "ia32.h"
 
 /******************** Public Typedefs ********************/
+typedef struct _MM_CONTEXT
+{
+	PVOID reservedPage;
+} MM_CONTEXT, *PMM_CONTEXT;
 
 /******************** Public Constants ********************/
 
 /******************** Public Variables ********************/
 
 /******************** Public Prototypes ********************/
-NTSTATUS MemManage_changeMemoryProt(PEPROCESS process, PUINT8 baseAddress, SIZE_T size, BOOLEAN writable, BOOLEAN executable);
-NTSTATUS MemManage_readVirtualAddress(PEPROCESS targetProcess, void* sourceVA, SIZE_T size, void* targetVA);
-NTSTATUS MemManage_writeVirtualAddress(PEPROCESS targetProcess, void* targetVA, SIZE_T size, void* sourceVA);
-NTSTATUS MemManage_getPhysFromVirtual(PEPROCESS targetProcess, PVOID virtualAddress, PHYSICAL_ADDRESS* physicalAddress);
-NTSTATUS MemManage_getPTEPhysAddressFromVA(PEPROCESS targetProcess, PVOID virtualAddress, PHYSICAL_ADDRESS* physPTE);
-UINT64 MemManage_getPageTableBase(PEPROCESS process);
-NTSTATUS MemManage_readPhysicalAddress(PHYSICAL_ADDRESS sourceAddress, SIZE_T size, void* targetAddress);
-NTSTATUS MemManage_writePhysicalAddress(PHYSICAL_ADDRESS targetAddress, SIZE_T size, void* sourceAddress);
+NTSTATUS MemManage_init(PMM_CONTEXT context, CR3 hostCR3);
